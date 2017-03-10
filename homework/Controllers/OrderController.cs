0ff9a2db0ea2bff0741dd78b852638016f2d8fc3
chi1103137225 <1103137225@gm.kuas.edu.tw>
2017-03-10 16:16:00 +0800

    update

---
+++ homework/Controllers/OrderController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace homework.Controllers
+{
+    public class OrderController : Controller
+    {
+        //
+        // GET: /Order/
+
+        public ActionResult Index()
+        {
+            Models.orderService orderService = new Models.orderService();
+            var order = orderService.GetOrderById("111");
+            ViewBag.CustId = order.CustId;
+            ViewBag.CustName = order.CustName;
+            return View();
+        }
+
+    }
+}
