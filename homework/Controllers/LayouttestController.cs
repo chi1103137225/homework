ac68786aa115493c1a9ff19492ac75e513ebd6ee
1103137225 <1103137225@gm.kuas.edu.tw>
2017-03-31 14:17:08 +0800

    106-03-31

---
+++ homework/Controllers/LayouttestController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace homework.Controllers
+{
+    public class LayouttestController : Controller
+    {
+        //
+        // GET: /Layouttest/
+
+        /// <summary>
+        /// 測試主板頁面
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
